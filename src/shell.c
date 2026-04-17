261d0abb8a7535208050043f54f84d7b2017d9f3
Ashutosh Jha <ashutoshjhaiitm@gmail.com>
2026-04-17 23:24:39 +0530

    run time error

--- src/shell.c
+++ src/shell.c
@@ -1,35 +1,3 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-struct SimpleCommand{
-	int _numberOfAvailableArguments;
-	int _numberOfArguments;
-	char ** _arguments;
-	SimpleCommand();
-	void insertArgument( char*argument);
-};
-
-struct Command{
-	int numberOfAvailableSimpleCommands;
-	int numberOfSimpleCommands;
-	SimpleCommand ** _simpleCommands;
-	char * _outFile;
-	char * _inputFile;
-	char * _errFile;
-	int _background;
-	
-	void prompt();
-	void print();
-	void execute();
-	void clear();
-
-	Command();
-	void insertSimpleCommand (SimpleCommand*simplecommand);
-
-	static Command _currentCommand;
-	static SimpleCommand *_currentSimpleCommand;
-
-
-};
-
