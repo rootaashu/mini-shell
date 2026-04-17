a716a2c4e568287e36e42a808cde0579b1414192
Ashutosh Jha <ashutoshjhaiitm@gmail.com>
2026-04-17 22:53:01 +0530

    added the file system part

--- src/shell.c
+++ src/shell.c
@@ -1,1 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct SimpleCommand{
+	int _numberOfAvailableArguments;
+	int _numberOfArguments;
+	char ** _arguments;
+	SimpleCommand();
+	void insertArgument( char*argument);
+};
+
+struct Command{
+	int numberOfAvailableSimpleCommands;
+	int numberOfSimpleCommands;
+	SimpleCommand ** _simpleCommands;
+	char * _outFile;
+	char * _inputFile;
+	char * _errFile;
+	int _background;
+};
+
