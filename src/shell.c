66f1d20b0415ff08df2d860c9376a6b2fc106700
Ashutosh Jha <ashutoshjhaiitm@gmail.com>
2026-04-18 23:26:47 +0530

    added globals and history managing function

--- src/shell.c
+++ src/shell.c
@@ -8,10 +8,41 @@
 #include <sys/wait.h> // process synchronization 
 #include <sys/types.h> // defines the system data types
 
-#define MAX_INPUT 1024; 
-#define MAX_ARGS 128;
-#define MEX_HISTORY 100;
-#define MAX_PIPES 16;
+#define MAX_INPUT 1024
+#define MAX_ARGS 128
+#define MAX_HISTORY 100
+#define MAX_PIPES 16
+
+static char hostory[MAX_HISTORY][MAX_INPUT];
+static int history_count = 0;
+static pid_t foreground_pid = -1;
+
+static void sigint_handler( int sig){
+	(void)sig;
+	if (foreground_pid > 0)
+		kill(foreground_pid, SIGINT);
+}
+
+static void sigchld_handler(int sig){
+	(void)sig;
+	while (waitpid(-1, NULL, WNOHANG) > 0);
+}
+
+static void add_history(const char *line){
+	if(strlen(line) == 0) return ;
+	if(history_count < MAX_HISTORY){
+		strncpy(history[history_count++], line, MAX_INPUT - 1);
+	}else {
+		memmove(history[0], history[1], sizeof(history[0] * (MAX_HISTORY - 1)));
+		strncpy(history[MAX_HISTORY - 1], line , MAX_INPUT - 1)
+	}
+}
+
+static void print_history( void){
+	for (int i=0; i<history_count; i++)
+		printf("%3d  %s/n", i+1, history[i]);
+}
+
 
 int main (){
 
