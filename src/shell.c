0e417373ec0384218914c88c969a61dac52cfe98
Ashutosh Jha <ashutoshjhaiitm@gmail.com>
2026-04-18 22:19:17 +0530

    added the required libraries

--- src/shell.c
+++ src/shell.c
@@ -1,3 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h> // I am using this for unix system calling 
+#include <fcntl.h> // using this for the file controlling 
+#include <errno.h> // gives proper error msg if found 
+#include <signal.h> // it controls and prevents the shell from exiting 
+#include <sys/wait.h> // process synchronization 
+#include <sys/types.h> // defines the system data types
+
+
