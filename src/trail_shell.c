0fca06eb1f47e7284aaeb7f09c2ea6fb3e70865d
Ashutosh Jha <ashutoshjhaiitm@gmail.com>
2026-04-22 01:36:36 +0530

    some more addtions to the program

--- src/trail_shell.c
+++ src/trail_shell.c
@@ -2,7 +2,23 @@
 #include <stdlib.h>
 
 #define LSH_RL_BUFSIZE 1024
+/*
+char *lsh_read_line(void)
+{
+	char *line = NULL;
+	size_t bufsize = 0;
+	if(getline(&line, &bufsize, stdin) == -1){
+	if (feof(stdin)){
+	exit(EXIT_SUCCESS);
+	}else{
+	perror("readline:");
+	exit(EXIT_FAILURE);
+	}
+	}
+	return line;
+	}
 
+*/
 char *lsh_read_line(void)
 {
 	int bufsize = LSH_RL_BUFSIZE ;
@@ -35,7 +51,7 @@ char *lsh_read_line(void)
 		}
 	}
 }
-}
+
 void lsh_loop(void)
 {
 	char *line;
