8a615274bb97c392b3d0813f0d066b1dad1a906e
Ashutosh Jha <ashutoshjhaiitm@gmail.com>
2026-04-22 01:14:26 +0530

    defined the basic loops for the program

--- src/trail_shell.c
+++ src/trail_shell.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
-int main(){
-	printf("Hello this is the trial shell program i am trying to build");
-	return 0;
+#include <stdlib.h>
+void lsh_loop(void)
+{
+	char *line;
+	char **args;
+	int status;
+
+	do{
+		printf(">");
+		line = lsh_read_line();
+		args = lsh_split_line(line);
+		status = lsh_execute(args);
+
+		free(line);
+		free(args);
+	}while (status);
+}
+
+
+
+
+
+
+int main(int argc, char **argv){
+	lsh_loop();
+	return EXIT_SUCCESS;
+
 }
