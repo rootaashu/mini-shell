ad59a19b3d168f5c74a692b80a7590b29cae778b
Ashutosh Jha <ashutoshjhaiitm@gmail.com>
2026-04-22 01:31:30 +0530

    fixed the running issue

--- src/trail_shell.c
+++ src/trail_shell.c
@@ -5,18 +5,18 @@
 
 char *lsh_read_line(void)
 {
-	int bufsize = LSH_RL_BURSIZE ;
+	int bufsize = LSH_RL_BUFSIZE ;
 	int position = 0;
 	char *buffer = malloc(sizeof(char) * bufsize);
 	int c ;
 
-	if(!butter){
+	if(!buffer){
 		fprintf(stderr, "lsh: allocation error\n");
 		exit(EXIT_FAILURE);
 	}
 	while(1){
 		c = getchar();
-		if (c==EOF || c = '\n'){
+		if (c==EOF || c == '\n'){
 			buffer[position] = '\0';
 			return buffer;
 		}else{
