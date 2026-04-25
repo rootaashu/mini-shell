03eb8315a03c5e3cb91d5c80f2e7493d64866bef
Ashutosh Jha <ashutoshjhaiitm@gmail.com>
2026-04-25 22:12:10 +0530

    Added built-in commands

--- src/trail_shell.c
+++ src/trail_shell.c
@@ -20,10 +20,41 @@ int(*builtin_func[]) (char **) = {
 	&lsh_exit
 };
 int lsh_num_builtins(){
-	return sizeof(builtin_str)
+	return sizeof(builtin_str) / sizeof(char *);
 }
 
+int lsh_cd(char **args)
+{
+	if (args[1] == NULL){
+		fprintf(stderr, "lsh : expected argument to \"cd\"\n");
+	}else{
+		if chdir(args[1] != 0){
+			perror("lsh");
+		}
+	}
+	return 1;
+}
 
+int lsh_help(char **args)
+{
+	int i;
+	printf("Ashutosh LSH\n");
+	printf("Type program names and arguments and hit enter.\n");
+	printf("The following the built-in functions:\n");
+
+	for (i =0; i<lsh_num_builtins(); i++){
+
+	printf(" %s \n",builtin_str[i]);
+
+	}
+	printf("Use the man command for the information on other programs.\n");
+	return 1;
+}
+
+int lsh_exit(char **args)
+{
+	return 1;
+}
 
 #define LSH_RL_BUFSIZE 1024
 
