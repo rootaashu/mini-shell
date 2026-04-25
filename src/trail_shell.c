3a1a9c53b3b26004dd75117334307fe05bef7c7e
Ashutosh Jha <ashutoshjhaiitm@gmail.com>
2026-04-25 22:16:07 +0530

    made the built-in commands executable and connected to the other commands

--- src/trail_shell.c
+++ src/trail_shell.c
@@ -184,7 +184,19 @@ void lsh_loop(void)
 	}while (status);
 }
 
-
+int lsh_execute(char **args)
+{
+	int i;
+	if(args[0] == NULL){
+		return 1;
+	}
+	for ( i=0; i < lsh_num_builtins(); i++){
+		if (strcmp(args[0], builtin_str[i]) == 0){
+			return (*builtin_func[i](args);
+			}
+		}
+	return lsh_launch(args);
+}
 
 
 
